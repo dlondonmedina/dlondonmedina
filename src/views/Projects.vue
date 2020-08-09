1bd5fcd7c4470022b9df208ff7edeb3ed6737ffc
Dylan Medina <medinady@seattleu.edu>
2020-08-08 21:47:38 -0700

    fix links

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="projects">
     <h1>This is an projects page</h1>
+    <div id="teaching">More content to follow</div>
+    <div id="developing">More content to follow</div>
+    <div id="writing">More content to follow</div>
   </div>
 </template>
